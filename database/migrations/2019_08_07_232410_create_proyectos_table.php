fa75fec14cd0c108305975609d50015011a6cb5b
Patrick Ciudad <PatrickWCity@gmail.com>
2019-08-09 18:13:39 -0400

    Tabla de Proyectos

---
+++ database/migrations/2019_08_07_232410_create_proyectos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProyectosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('proyectos', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment('Identificador de Proyecto');
+            $table->string('nombre', 60)->unique()->comment('Nombre de Proyecto');
+            $table->string('descripcion')->nullable()->default(null)->comment('Descripción de Proyecto');
+            $table->string('imagen')->comment('Imagen de Proyecto');
+            $table->boolean('estado')->comment('Estado de Proyecto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('proyectos');
+    }
+}
